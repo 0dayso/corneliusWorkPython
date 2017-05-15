6365cb3e37800345fa3daa4e5c58d0548a5ea4ed
baojj <baojj@shinemo.com>
2017-05-15 10:28:19 +0800

    update

--- com/bjj/reptile/novel/bi_qu/http_lib.py
+++ com/bjj/reptile/novel/bi_qu/http_lib.py
@@ -9,6 +9,13 @@ sys.setdefaultencoding('utf-8')
 
 url1 = "http://www.i7wx.com/book/8/8294/"
 url2 = "http://www.i7wx.com/book/8/8294/500741.html"
+text = "E:\\workspace\\corneliusWorkPython\\com\\bjj\\reptile\\novel\\bi_qu\\test.txt"
+error = "E:\\workspace\\corneliusWorkPython\\com\\bjj\\reptile\\novel\\bi_qu\\error.txt"
+
+f = open(text, "r+")
+f1 = open(error, "r+")
+
+
 
 
 
@@ -26,29 +33,53 @@ def getAllChapter(url):
 def getSingleChapterContent(url):
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
-    result =  response.read().decode("gbk")
+    result = response.read().decode("gbk")
     pattern = re.compile('<div id="content">(.*?)</div>', re.S)
     items = re.findall(pattern, result)
-    return items[0].replace("<br/>","").replace("&nbsp;","")
-    # for item in items:
-    #     print item.replace("<br/>","").replace("&nbsp;","")
+    return items[0].replace("<br/>", "").replace("&nbsp;", "")
+
+def isPageError(index, title, url):
+
+    try:
+        request = urllib2.Request(url)
+        response = urllib2.urlopen(request)
+        result = response.read().decode("gbk")
+        pattern = re.compile('<div id="content">(.*?)</div>', re.S)
+        items = re.findall(pattern, result)
+        content = items[0].replace("<br/>", "").replace("&nbsp;", "")
+        f.write(title + "\n")
+        f.write(content + "\n")
+    except Exception, e:
+        print Exception, ":", e
+        index+=1
+        if index<5:
+            isPageError(index, title, url)
+    return False
+
+
 
 
 
 if __name__ == '__main__':
     li1 = getAllChapter(url1)
-    f = open("E:\\workspace\\corneliusWorkPython\\com\\bjj\\reptile\\novel\\bi_qu\\test.txt", "r+")
     i = 1
     for lis in li1:
         url3 = "http://www.i7wx.com/book/8/8294/" + lis[0]
-        content = getSingleChapterContent(url3)
-        # print "开始写入： " + lis[1]
-        print i
-        f.write(lis[1] + "\n")
-        f.write(content + "\n")
-        time.sleep(2)
-        i+=1
+        try:
+            content1 = getSingleChapterContent(url3)
+            # print "开始写入： " + lis[1]
+            print i
+            f.write(lis[1] + "\n")
+            f.write(content1 + "\n")
+            time.sleep(2)
+            i+=1
+        except Exception, e:
+            print Exception, " : ", e
+            if not isPageError(1, lis[1], url3):
+                f1.write("[\"" + lis[0] + "\",[\"" + lis[1] + "\"],")
+
     f.close()
+    f1.close()
     # content = getSingleChapterContent(url2)
     # f.write(content)
     # f.close()
--- com/testCode/test.py
+++ com/testCode/test.py
@@ -1,143 +1,38 @@
 #! usr/bin/python
 # coding:UTF-8
 import re
-
-content = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7"/>
-<title>大明虎贲 第一章 从天而降的你_爱奇文学</title>
-<meta name="keywords" content="第一章 从天而降的你,大明虎贲最新章节爱奇文学提供" />
-<meta name="description" content="此页面是大明虎贲 第一章 从天而降的你,爱奇文学提供的最新章节" />
-<meta name ="mobile-agent" content="format=xhtml; url=http://m.i7wx.com/8/8294/500741.html" />
-<link rel="stylesheet" type="text/css" href="/skin/52mb.css" />
-<script type="text/javascript" src="/skin/jquery.min.js"></script>
-<script type="text/javascript" src="/skin/52mb.js"></script>
-<script type="text/javascript">uaredirect("http://m.i7wx.com/8/8294/500741.html");</script>
-<script src="/skin/base.js" type="text/javascript"></script>
-<script type="text/javascript">
-<!--
-var preview_page = "./";
-var next_page = "500749.html";
-var index_page = "./";
-var article_id = "8294";
-var chapter_id = "500741";
-function jumpPage() {
- var event = document.all ? window.event : arguments[0];
- if (event.keyCode == 37) document.location = preview_page;
- if (event.keyCode == 39) document.location = next_page;
- if (event.keyCode == 13) document.location = index_page;
-}
-document.onkeydown=jumpPage;
--->
-</script>
-</head>
-<body onload="T.read.init();" class="clo_bg" style="-moz-user-select:none;-webkit-user-select:none;" onselectstart="return false">
-<div id="header">
-  <div class="top"> <span class="loginSide"><a href="javascript:T.setHome();">将本站页</a> | <a href="javascript:T.addBookMark();">收站</a></span>
-    <ul>
-      <script>login();</script>
-    </ul>
-  </div>
-  <div class="readNav"> <span>当前位置：<a href="/">首页</a>><a href="/list/4_1.html">历史军事</a>><a href="/book/8294.html">大明</a>> <em>第一的你</em></span>
-    <div class="search">
-      <form id="search" name="t_frmsearch" method="post" action="/modules/article/search.php">
-        <span class="searchBox"><span id="seartype" class="seartype" onclick="ctype()" title="点击切换搜索类型" style="line-height:normal;">书名</span>
-        <input name="searchkey" type="text" value="请您少世" autocomplete="off" title="请您少世" onFocus="if(this.value==this.title) this.value='';" onBlur="if(this.value=='') this.value=this.title;"/>
-        </span>
-        <button type="submit" class="serBtn">搜索</button>
-        <select id="seartype2" name="searchtype" style="display:none">
-          <option value="articlename" selected>书名</option>
-          <option value="author">作者</option>
-        </select>
-      </form>
-    </div>
-  </div>
-</div>
-<div id="container">
-  <div class="comments_r">
-    <div class="h3">
-      <div class="m_12_t">
-        <div class="select_t"> <span id="select_color_txt" class="select_t_bg"><em>默认冷灰</em></span><a href="javascript:;" id="select_color" class="select_t_r"></a> </div>
-        <div class="text_bg hidden">
-          <ul>
-          </ul>
-        </div>
-        <div class="select_t"> <span id="select_size_txt" class="select_t_bg"><em>24号文字</em></span><a href="javascript:;" id="select_size" class="select_t_r"></a> </div>
-        <div class="text_bg2 hidden">
-          <ul>
-          </ul>
-        </div>
-        <div class="select_t"> <span id="select_family_txt" class="select_t_bg"><em>方正启体</em></span><a href="javascript:;" id="select_family" class="select_t_r"></a> </div>
-        <div class="text_bg3 hidden">
-          <ul>
-          </ul>
-        </div>
-      </div>
-      <ul class="links">
-        <li><a href="#" onclick="addbookcase(8294,0);">加入书架</a> | </li>
-        <li><a href="#" onclick="addbookcase(8294,500741);">标记书签</a> | </li>
-        <li><a href="#" onclick="vote(8294);">投推荐票</a> | </li>
-        <li><a href="/modules/article/txtarticle.php?id=8294" target="_blank">下载本书</a> | </li>
-        <li><a href="#" onclick="report('《大明虎贲》的章节 第一章 从天而降的你 有错误')">报错求片</a> | </li>
-        <li><a href="/book/8/8294/">作品目录</a> | </li>
-        <li><a href="/book/8294.html">返回封面</a></li>
-      </ul>
-    </div>
-  </div>
-  <div id="center">
-    <div class="title">
-      <h1>第一章你</h1>
-      作者:<span><a href="/modules/article/authorarticle.php?author=挑灯看剑" rel="nofollow">挑灯看剑</a></span>本书字数:<span><script>zishu('8294')</script>K</span>更新时间:<span><script>uptime('8294')</script></span> </div>
-    <div class="content_ad top_b"><div style="margin:0;padding:0;width: 920px;/* height:345px; */overflow:hidden;background-color:#fff;margin:0 auto;">
-<div style="margin:0 0 5px;padding:0;width: 980px;height:250px;overflow:hidden;background-color:#fff;">
-<div style="margin:0;padding:0;height:250px;overflow:hidden;background-color:#fff;float:left;">
-<script>style_1();</script>
-</div>
-<div style="margin:0;padding:0;width: 320px;height:250px;overflow:hidden;background-color:#fff;float:left;">
-<script>style_2();</script>
-</div>
-<div style="margin:0;padding:0;width: 300px;height: 250px;overflow:hidden;background-color:#fff;">
-<script>style_3();</script>
-</div>
-</div>
-</div> </div>
-    <div id="content">&nbsp;&nbsp;&nbsp;&nbsp;“啊…………”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;噗通！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;小花园里，一位小姐正在园中舞剑，几名家奴院丁围在四周围，簇拥着一位年迈的老者坐在一旁观赏着，不断的叫着好，就在这个时候，一声凄厉的惨呼，从天上传了下来，紧接着一个黑影狠狠的砸落在了花园的小湖之中，溅起了老高的浪花。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;此时正是三更时分，大晚上的，突如其来这么一下子，所有人都吓了一跳，老者腾身站起，喝道：“过去看看，到底是发生了什么？”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;一个家丁连忙向着小湖跑了过去，只见一个人正在挣扎着向着湖边游了过来。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;“什么人？”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;有人喝道。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;“还用问吗？三更半夜闯入咱们宅子里，自然不是草寇就是盗贼了，正经人谁会偷偷潜入咱们孙府？”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;院丁乙在一旁喝道。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;这个时候，袁啸已经从湖里爬出来了。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸本来就是一个共和国的特种兵，正在西南进行着缉毒任务，上面得到了情报，说是一个贩毒团伙从米安国进入了共和国的西南边境，必须要立即进行围剿，以免他们携带的大量毒品，进入境内，流毒人民。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸所在的小分队十几个人迅速出动执行任务，哪里知道，这个情报竟然是假的，小分队刚刚抵达指定位置，就遭遇到了武装分子的袭击，上百名武装分子向他们发动了围攻，袁啸只能带着弟兄们突围了。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;结果，袁啸成功的引开了武装分子，使得弟兄们脱离险境，自己却是再也难以甩掉后面的追兵了，为了不当俘虏，只能咬着牙，从悬崖上跳了下去。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;不过，这一跳，竟然让他感觉到了如此的漫长，多长时间，谁也不知道，只知道自己飘荡了好长时间，如果世界上真的有这么深的峡谷的话，那绝对可以创造吉尼斯世界记录了！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;终于，袁啸的身体落到了下面，还好，不是地面，而是一个湖泊，下面数米深的湖水救了他的小命。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸急忙从湖里爬了出来，浑身酸痛，即便是有着湖水接着，巨大的冲击力，也不是一般人能够承受的了得！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;刚刚爬了上来，袁啸就被一群人给狠狠的摁在了地上，手中的突击步枪也被人给打落了！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸心头一叹，特么的，倒霉了，即便是跳崖，都没有能够摆脱被俘的下场啊！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸被几个人给摁着来到了花园中间，一个院丁说道：“老爷，就是这个家伙夜闯咱们孙府，不是草寇，也是盗匪，咱们这就把他给捆起来，送到县衙去！”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸一愣，老爷？孙府？还县衙？特么的，现在的中国哪里还有什么县衙？自己难道出现幻觉了？<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸缓缓的抬起头来，更加的有些迷惑了，眼前的所有人都是一身古代的装束，周围是一个巨大的花园，这究竟是什么地方？<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;中间的老者摆摆手，问道：“年轻人，你到底是干什么的？夜入我孙府，意欲何为？”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸双手狠狠一挣，胳膊从家丁的手里脱了出来，朗声道：“什么，盗贼？我不是盗贼，我乃是共和国的军人！因为意外，不得不跳崖求生，结果落入了贵府的这个水池里。”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;“放屁！”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;一旁的家丁怒吼道：“好你个贼子，撒谎都不会撒，这里是县城，这里是平原，方圆数百里都看不到一个山头，哪里来的悬崖？还特么的跳崖求生，纯粹放屁！”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;老者的脸色也沉了下来，不悦道：“年轻人，老夫念你年纪轻轻，不愿意将你送入县衙法办，只要你老实说，我就可以放了你，不然的话……”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸心里一阵郁闷，特么的，到底怎么搞的，自己跳个崖，难道都跟坐飞机没有区别，竟然从深山飘到了平原！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸将两手一摊，答道：“老爷子，我说的可句句都是实话，你看我这身装束，难道还看不出我是一个军人吗？”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸以为自己一身迷彩装，全身都是武器装备，自然所有人都会认为自己是军人了。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;孰料，周围的家丁们包括老者都是哈哈大笑起来，家丁们更是笑得直不起腰。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;军人，特么的，自从出了娘胎，就没有见过这幅尊容的当兵的！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;老者摇摇头，笑道：“笑话了，老夫掌管大明兵权愈十数年之久，与鞑子、鞑靼交锋数十次，还真的从来没有见过你这样打扮的军人……”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;额……<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸一摸额头，大明兵权？我去啊！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;“老人家，莫非你们在这里演戏？如果我打扰了你们拍摄或者演戏，我向你们道歉……”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;老者脸色一沉，喝道：“胡闹，谁在跟你演戏！”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;一旁的家丁更是怒不可遏，其中的一个家丁抄起一根棍子向着袁啸的膝盖弯砸来，喝道：“混蛋！老爷乃是当年堂堂的帝师，兵部尚书！你竟然拿个戏子来侮辱我们老爷，看打！”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;我擦！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸当时就怒了，老子还没有干啥呢，就被你们给摁在地上了，现在事情还没有弄明白，又给老子一棍子，你当老子好脾气吗？<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸右腿一抬，向着棍子踢去！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;咔嚓！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;小臂粗细的木棒从中应声折断，袁啸的右腿去势不衰，正中家丁的小肚子上！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;啊！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;家丁向着后面接连倒退了七八步，一屁股坐在了地上，惨哼不已。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;“保护老爷，小姐！”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;领头的家丁大惊失色，连忙招呼人将老者跟小姐护在了中间，其他的十多个人向着袁啸围了上来。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;袁啸微微皱眉，喝道：“老人家，我真的不知道是怎么回事到的这里，如果我有恶意的话，只怕你们这里已经趴下至少十几个人了，我真的是军人，这里到底是什么地方？”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;领头的家丁喝道：“管你是不是当兵的，就是一省的督抚在我们孙府，也不敢放肆，何况是你！上，给我将他拿下！”<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;十多个家丁再次围了上来，向着袁啸发动了进攻，袁啸无奈，只能先将这些人给打趴下再说了！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;身形转动间，拳脚齐飞，花园里不断传出惨叫之声。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;远处的老者不断的点着头，嘴中说着：“嗯，不错，这个年轻人，这身本事真的不含糊，即便是放在明军之中，也绝对是首屈一指的高手了，不错——住手！给我全部住手！”</div>
-  </div>
-  <div class="jump"><span>(←快捷键)</span> <a href="./"><<上一章</a> <a href="#" onclick="vote(8294);">投推荐票</a> <a href="./">回目录</a> <a href="#" onclick="addbookcase(8294,500741);">标记书签</a> <a href="500749.html">下一章>></a> <span>(快捷键→)</span></div>
-  <div class="clearfix"></div>
-  <div class="bottoma"><script>bottoma();</script></div>
-  <div id="footer">
-    <p class="copyright">本欣赏。<br />
-      Copyright  2014ved <script>tj();beian();dl();</script>
-    <p>
-  </div>
-</div>
-</body>
-</html>
-
-"""
-
-content1 = """
-    <div id="content">！<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;身形转动间，拳脚齐飞，花园里不断传出惨叫之声。<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;远处的老者不断的点着头，嘴中说着：“嗯，不错，这个年轻人，这身本事真的不含糊，即便是放在明军之中，也绝对是首屈一指的高手了，不错——住手！给我全部住手！”</div>
+import urllib2
 
 
-"""
 
-pattern = re.compile('<div id="content">(.*?)</div>', re.S)
-items = re.findall(pattern, content)
-# li = []
+text = "E:\\workspace\\corneliusWorkPython\\com\\bjj\\reptile\\novel\\bi_qu\\test.txt"
+f = open(text, "r+")
 
-# print items[0].replace("<br/>","").replace("&nbsp;","")
 
-f = open("E:\\workspace\\corneliusWorkPython\\com\\bjj\\reptile\\novel\\test.txt", "r+")
-f.write("你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是你是" + "\n")
-f.write("2222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222" + "\t")
-# f.write(content)
+def isPageError(index, title, url):
 
-f.close()
+  try:
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    result = response.read().decode("gbk")
+    pattern = re.compile('<div id="content">(.*?)</div>', re.S)
+    items = re.findall(pattern, result)
+    content = items[0].replace("<br/>", "").replace("&nbsp;", "")
+    f.write(title + "\n")
+    f.write(content + "\n")
+  except Exception, e:
+    print Exception, ":", e
+    if index<5:
+      index+=1
+      isPageError(index, title, url)
+  return False
+
+
+
+if __name__ == '__main__':
+    print isPageError(1,"123", "http://www.baidu.com")
+
+
 
 
 
