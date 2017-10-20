7a464e060d0bdb3b792382c0cee40542440f7671
baojunjun <baojj@shinemo.com>
2017-10-20 14:12:09 +0800

    chinaz

--- com/bjj/reptile/photo/chinaz/http_response.py
+++ com/bjj/reptile/photo/chinaz/http_response.py
@@ -3,6 +3,7 @@
 import re
 import urllib2
 import time
+import requests
 
 name = 'chiqiangdongzuotupian'
 pages = 7
@@ -42,6 +43,31 @@ def writeToTxt(url):
         bu = item[0] + ', ' + item[1] + '\n'
         f.write(bu)
     f.close()
+
+def downloadPic(imageUrl, filePath):
+    r = requests.get(imageUrl)
+    with open(filePath, "wb") as code:
+        code.write(r.content)
+
+
+# if __name__ == '__main__':
+#     path = 'D:\\0Bjj\Photo\\chinaz\\jiaotonggongju\\'
+#
+#     f = open('D:\\test\\chinaz\\jiaotonggongju.txt', 'r')
+#     index = 0
+#     for line in f.readlines():
+#         if index < 5:
+#             line = line.split(',')
+#             name = line[0].decode('utf-8')
+#             url = line[1].replace('\n','').replace(' ','')
+#             print url
+#             print path + name + '.rar'
+#             downloadPic(url, path + "123" + '.rar')
+#             index += 1
+#             time.sleep(2)
+#         else:
+#             break
+#     f.close()
 
 if __name__ == '__main__':
     for i in range(1, pages + 1):
